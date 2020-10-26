5409f694c5ff3469da2f3012893fa3d83405538d
Evan Gerweck <gerweckevan@gmail.com>
2020-10-25 22:20:59 -0600

    added the script.js file

---
+++ Develop/script.js
@@ -0,0 +1,107 @@
+//set up work day array
+let workDay = {
+  "8 AM": "",
+  "9 AM": "",
+  "10 AM": "",
+  "11 AM": "",
+  "12 PM": "",
+  "1 PM": "",
+  "2 PM": "",
+  "3 PM": "",
+  "4 PM": "",
+  "5 PM": "",
+};
+//update calendar tasks and get new item of work day so it can go to diffrent day if not parse it
+$(document).ready(function () {
+  if (!localStorage.getItem("workDay")) {
+    updateCalendarTasks(workDay);
+  } else {
+    updateCalendarTasks(JSON.parse(localStorage.getItem("workDay")));
+  }
+});
+//set up the date today for the (header refrence this)
+//using moment.js in the file to get correct time and day and their lingo copyied for date time
+$("#date-today h6").text(
+  moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm:ss a")
+);
+//setting up
+let counter = 1;
+for (const property in workDay) {
+  let textEntry = "#text-entry" + counter;
+  $(textEntry).text(workDay[property]);
+  let timeId = "#time" + counter;
+  let presentHour = moment().hour();
+  let timeString = $(timeId).text();
+  let timeNumber = hourNumberFromHourString(timeString);
+  if (timeNumber < presentHour) {
+    $(textEntry).addClass("past-hour");
+  } else if (timeNumber > presentHour) {
+    $(textEntry).addClass("future-hour");
+  } else {
+    $(textEntry).addClass("present-hour");
+  }
+  counter++;
+}
+//create a click function save the values of value and hour
+$("button").click(function () {
+  value = $(this).siblings("textarea").val();
+  hourString = $(this).siblings("div").text();
+
+  saveSchedule(hourString, value);
+});
+//made a function that converts string to array
+function hourNumberFromHourString(hourString) {
+  switch (hourString) {
+    case "8 AM":
+      return 8;
+    case "9 AM":
+      return 9;
+    case "10 AM":
+      return 10;
+    case "11 AM":
+      return 11;
+    case "12 PM":
+      return 12;
+    case "1 PM":
+      return 13;
+    case "2 PM":
+      return 14;
+    case "3 PM":
+      return 15;
+    case "4 PM":
+      return 16;
+    case "5 PM":
+      return 17;
+  }
+}
+
+function loadCorrectDataset() {
+  result = localStorage.getItem("workDay");
+  return result ? result : workDay;
+}
+//intialize and save to local storage
+function initializeLocalStorage() {
+  localStorage.setItem("workDay", JSON.stringify(workDay));
+}
+
+function saveToLocalStorage(dayObj) {
+  localStorage.setItem("workDay", JSON.stringify(dayObj));
+}
+//save value in hour string of the work day
+function saveSchedule(hourString, val) {
+  if (!localStorage.getItem("workDay")) {
+    initializeLocalStorage();
+  }
+
+  let workHours = JSON.parse(localStorage.getItem("workDay"));
+  workHours[hourString] = val;
+
+  saveToLocalStorage(workHours);
+}
+
+function updateCalendarTasks(dayObject) {
+  $(".calendar-row").each(function (index) {
+    let res = $(this).children("div");
+    $(this).children("textarea").text(dayObject[res.text()]);
+  });
+}
